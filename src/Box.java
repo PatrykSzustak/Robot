c46305aa7a806df03c506afe4cb9175b364b014d
Unknown <patryk.szustak6@gmail.com>
2018-03-20 23:59:13 +0100

    Official version Robot

---
+++ src/Box.java
@@ -0,0 +1,14 @@
+public class Box {
+
+    private static final int MAX_CAPACITY = 15;
+    private boolean spaceLeft = true;
+
+    public boolean isSpaceLeft(int box) {
+
+        if (box == MAX_CAPACITY) spaceLeft = false;
+        else spaceLeft = true;
+
+        return spaceLeft;
+    }
+
+}
